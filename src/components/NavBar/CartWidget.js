b456924c1da01aba1bb636b6466bc9126c1cfd39
Franco_Turano <francoturano777@gmail.com>
2022-11-09 00:44:12 -0300

    hardcodeamos el numero del carrito

---
+++ src/components/NavBar/CartWidget.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./CartWidget.css";
+function CartWidget() {
+  return (
+    <div className='carrito'>
+      <img className='carrito-car' src="./imgs/carrito1.jpeg" alt="logoCarrito"/> 
+      <img className='carrito-num' src="./imgs/numero.webp" alt="numeroCarrito"/>
+    </div>
+  )
+}
+
+export default CartWidget
